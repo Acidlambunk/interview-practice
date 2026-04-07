f7a08d384d4f02c53a29c4168de2f7ae3fb47447
Acidlambunk <78156871+Acidlambunk@users.noreply.github.com>
2026-04-07 16:13:09 +0800

    Add: baseball-game - submission-17

---
+++ Data Structures & Algorithms/baseball-game/submission-17.py
@@ -0,0 +1,13 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        total = []
+        for n in operations:
+            if n == '+':
+                total.append(int(total[-1])+int(total[-2]))
+            elif n == "D":
+                total.append(total[-1]*2)
+            elif n == "C":
+                total.pop()
+            else:
+                total.append(int(n))
+        return sum(total)
